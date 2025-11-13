1f9361b62140f132e1948abc49fccc896c1464e9
Rubén Montes Rodrigo <rubmonrod3@alu.edu.gva.es>
2025-11-13 17:49:06 +0100

    hjo

--- js/index.js
+++ js/index.js
@@ -1,19 +1,82 @@
 const main = document.getElementById('main');
 const welcome = document.getElementById('welcome');
+const login = document.getElementById('login');
+const inputEmail = document.getElementById('email');
+const spanError = document.getElementById('login-error');
+const inputSubmit = document.getElementById('login-submit');
 
-const welcomeTimeout = setTimeout(() => {
-    welcome.style.display = "none";
+const emailRegex = new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+
+
+// Welcome
+
+const onWelcomeClose = () => {
+    welcome.style.display = 'none';
     window.removeEventListener('keydown', onKeyDownWelcomeCallback);
-}, 5000);
+    login.style.display = 'block';
+    onLogin();
+}
+
 
 const onKeyDownWelcomeCallback = (e) => {
 
     if (e.key === 'F11' && e.ctrlKey){
-        welcome.style.display = "none";
         clearTimeout(welcomeTimeout);
-        window.removeEventListener('keydown', onKeyDownWelcomeCallback);
+        onWelcomeClose();
     }
 
 }
 
+
+const welcomeTimeout = setTimeout(() => {
+    onWelcomeClose();
+}, 5000);
+
 window.addEventListener('keydown', onKeyDownWelcomeCallback);
+
+//Login
+
+const validEmail = (email) => emailRegex.test(email);
+
+const onBlurEmail = (e) => {
+    const valid = validEmail(e.target.value);
+    spanError.style.visibility = valid ? 'hidden' : 'initial';
+    inputSubmit.disabled = !valid;
+
+    if (!valid){
+        inputEmail.select();
+    }
+}
+
+const onSubmitForm = (e) => {
+    e.preventDefault();
+    const email = inputEmail.value;
+
+    if (!validEmail(email)) return;
+
+    const stored = localStorage.getItem('users');
+    const data = stored != null ? JSON.parse(stored) : {};
+
+    const userData = {};
+
+    if (email in data){
+        userData = data[email];
+    }
+
+    if (!("email" in data)){
+        userData.email = email;
+    }
+
+    data[email] = userData;
+    
+    localStorage.setItem('user', JSON.stringify(data));
+    
+}
+
+const onLogin = () => {
+
+    login.addEventListener('submit', onSubmitForm);
+    inputEmail.addEventListener('blur', onBlurEmail);
+
+}
+
